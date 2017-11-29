583d194f4f9fd475bee5a31d0f0c9fc377735379
arjungupta48 <33134982+arjungupta48@users.noreply.github.com>
2017-11-29 16:52:15 +0530

    Added hide/show button

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import {StudentService} from '../student.service';
 import {IStudent} from '../student';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-student-list',
@@ -10,17 +11,37 @@ import {IStudent} from '../student';
 })
 
 export class StudentListComponent implements OnInit {
-  private students: IStudent[];
-
-  constructor(private _studentService: StudentService) { }
-
-  ngOnInit() {
-    this._studentService.getStudents()
-      .subscribe(resStudentData => {
-        this.students = resStudentData;
-      });
+  private students: Observable<IStudent>;
+  private showHide: boolean;
+  private showHideValue: string;
+  
+  constructor(private _studentService: StudentService) { 
+    this.students = this._studentService.data;
+    this.showHide = false;
+    this.showHideValue = "Show";
   }
 
+  ngOnInit() {    
+    if (!this.students) {
+      this._studentService.getStudents();
+    }
+  }
 
+  setValue() {
+    this.students = this._studentService.data;
+  }
 
+  canShowData() {
+    this.setValue();
+    this.showHide == true;
+    this.showHide = !this.showHide;
+    
+    if(this.showHideValue == "Show") {
+      this.showHideValue = "Hide";
+    }
+    else {
+      this.showHideValue = "Show";
+    }
+  }
+  
 }
