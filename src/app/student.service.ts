a85a8d7f66442132752fd6f1192bf94c877117f8
arjungupta48 <33134982+arjungupta48@users.noreply.github.com>
2017-12-11 18:29:58 +0530

    fix for hide/show response. Added data property

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {IStudent} from './student';
 import {Observable} from 'rxjs/Observable';
+import {IStudent} from './student';
 
 @Injectable()
 export class StudentService {
-  private _url: string = "assets/studentdata.json"
+  private _url: string = "assets/studentdata.json";
+  public data : Observable<IStudent>;
+
   constructor(private _http: Http) { }
 
-  getStudents(): Observable<IStudent[]> {
+  getStudents() {
     return this._http.get(this._url)
-      .map((response: Response) => response.json());
+               .map((response: Response) => response.json())
+               .subscribe(response => this.data = response);             
   }
+  
 }
 
 
